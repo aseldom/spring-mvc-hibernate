368f4dc2b217e5c74d1bd795713f23d05402d13c
aseldom <a.seldom@gmail.com>
2024-05-01 11:12:20 +0500

    add aspect

--- src/main/java/com/zaurtregulov/spring_mvc_hibernate/aspect/MyLoggingAspect.java
+++ src/main/java/com/zaurtregulov/spring_mvc_hibernate/aspect/MyLoggingAspect.java
@@ -1,4 +1,25 @@
 package com.zaurtregulov.spring_mvc_hibernate.aspect;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
 public class MyLoggingAspect {
+
+    @Around("execution(* com.zaurtregulov.spring_mvc_hibernate.dao.*.*(..))")
+
+    public Object aroundAllRepositoryMethods(ProceedingJoinPoint proceedingJoinPoint) throws Throwable{
+        MethodSignature methodSignature = (MethodSignature) proceedingJoinPoint.getSignature();
+        String name = methodSignature.getName();
+
+        System.out.println("Begin of method: " + name);
+        Object methodResult = proceedingJoinPoint.proceed();
+        System.out.println("End of method: " + name);
+
+        return methodResult;
+    }
 }
